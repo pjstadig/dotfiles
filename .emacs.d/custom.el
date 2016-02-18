fd67b240c7dd6a82186fbf847f5627f7becf397f
Paul Stadig <paul@stadig.name>
2016-02-18 09:05:50 -0500

    Don't have cider prompt for M-. symbol.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(sh-indentation 2))
+ '(sh-indentation 2)
+ '(cider-prompt-for-symbol nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
