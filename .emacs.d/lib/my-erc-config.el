5d0d9855e43a7c84fb89572c63c16f0b64e1c215
Paul Stadig <paul@stadig.name>
2018-10-18 13:55:18 -0400

    Define my/znc-password either way.

--- .emacs.d/lib/my-erc-config.el
+++ .emacs.d/lib/my-erc-config.el
@@ -2,9 +2,11 @@
   (interactive "Mserver: ")
   (let ((znc-password-file "~/.private/my-znc-password.el"))
     (if (file-exists-p znc-password-file)
-        (progn (load znc-password-file)
-               (erc-tls :server server
-                        :port "6697"
-                        :nick "paul"
-                        :password (concat "paul:" my/znc-password)))
-      (message "Failed to load ZNC password file."))))
+        (load znc-password-file)
+      (eval-when-compile
+        (defvar my/znc-password)
+        (message "Failed to load ZNC password file."))))
+  (erc-tls :server server
+           :port "6697"
+           :nick "paul"
+           :password (concat "paul:" my/znc-password)))
