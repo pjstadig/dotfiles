afc69598f71a6e6546ea4d60469e9b1281e2634f
Paul Stadig <paul@stadig.name>
2018-12-08 11:33:21 -0500

    Allow exwm to switch to any buffer in any workspace.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(exwm-layout-show-all-buffers t)
+ '(exwm-workspace-show-all-buffers t)
  '(package-selected-packages
    (quote
     (typo org-bullets markdown-mode writegood-mode visual-fill-column use-package paredit magit helm-projectile helm-ag exwm cider better-defaults)))
