b16e4634a36db6b5cda9d2efbe887167f9dfa804
Paul Stadig <paul@stadig.name>
2018-12-10 17:09:21 -0500

    Use a wrapper .app to set PATH for Emacs on OSX.

--- .emacs.d/lib/my-osx-config.el
+++
@@ -1,4 +0,0 @@
-;; OSX finder launched applications...environment...grumble.  If I
-;; want to be able to jack in with cider, it needs to be able to find
-;; lein on the path.  This is a no-op on nixos.
-(add-to-list 'exec-path (expand-file-name "~/bin"))
