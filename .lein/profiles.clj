53706f7f8a666ea3e5ed04e7d2a7721be2d4ceca
Paul Stadig <paul@stadig.name>
2019-10-09 09:52:57 -0400

    Update lein config.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,1 +1,5 @@
-{:user {:plugins [[lein-pprint "1.2.0"]]}}
+{:user {:dependencies [[pjstadig/humane-test-output "0.8.3"]]
+        :injections [(require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]
+        :plugins [[lein-ancient "0.6.12"]
+                  [lein-pprint "1.2.0"]]}}
