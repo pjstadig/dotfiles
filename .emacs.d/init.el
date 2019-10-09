8a8ae2cf465067ae57c796ce49c688e2bfb99c10
Paul Stadig <paul@stadig.name>
2019-10-09 09:48:40 -0400

    Use a local customization file.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,11 @@
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (load custom-file)
 
+(let ((local-custom-file (concat user-emacs-directory "custom-local.el")))
+  (when (file-exists-p local-custom-file)
+    (setq custom-file local-custom-file)
+    (load custom-file)))
+
 ;; Set fonts
 (set-face-attribute 'default nil :family "Fira Mono")
 (set-face-attribute 'fixed-pitch nil :family "Fira Mono")
