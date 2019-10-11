c06f48e3f416b979fff81859e287b4b305e9fb4a
Paul Stadig <paul@stadig.name>
2019-10-11 05:40:38 -0400

    Please help my eyes.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,9 +2,9 @@
 (load custom-file)
 
 ;; Set fonts
-(set-face-attribute 'default nil :family "Fira Mono")
-(set-face-attribute 'fixed-pitch nil :family "Fira Mono")
-(set-face-attribute 'variable-pitch nil :family "Fira Sans" :weight 'light)
+(set-face-attribute 'default nil :family "Fira Mono" :height 120)
+(set-face-attribute 'fixed-pitch nil :family "Fira Mono" :height 120)
+(set-face-attribute 'variable-pitch nil :family "Fira Sans" :weight 'light :height 120)
 
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 
