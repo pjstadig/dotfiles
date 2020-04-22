a25f909ae94e61bc29466d2392a82b04c98170df
Paul Stadig <paul@stadig.name>
2020-04-22 11:09:50 -0400

    Add some magit helpers from Toby.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -39,7 +39,40 @@
   :config (gnu-elpa-keyring-update))
 (use-package magit
   :ensure t
-  :bind ("C-c g" . magit-status))
+  :bind (("C-c g" . magit-status)
+         :map git-commit-mode-map
+         ("C-c l" . 'tc/insert-clubhouse-story-url)
+         ("C-c C-l" . 'tc/insert-clubhouse-story-url)
+         ("C-c a" . 'tc/insert-co-authored-by)
+         ("C-c C-a" . 'tc/insert-co-authored-by)))
+
+(defun tc/insert-clubhouse-story-url ()
+  "Looks forward in the buffer for a story branch, and uses the
+story id to generate and insert a url to the story."
+  (interactive)
+  (insert
+   (save-excursion
+     (when (re-search-forward "branch .*/ch\\([0-9]+\\)/")
+       (format "https://app.clubhouse.io/internal/story/%s/" (match-string 1))))))
+
+(defun tc/get-recent-git-authors (n)
+  "Returns an alpha sorted list of the unique authors of the last
+  n commits for the current git repo."
+  (let ((default-directory (file-name-directory (buffer-file-name))))
+    (split-string
+     (shell-command-to-string
+      (format "git log HEAD~%s.. --pretty='%%an <%%ae>' | sort | uniq" n))
+     "\n")))
+
+(defun tc/insert-co-authored-by ()
+  "Inserts a Co-authored-by header from a selction from authors
+  for the 50 most recent commits."
+  (interactive)
+  (insert (format "Co-authored-by: %s"
+                  (completing-read "co-author: "
+                                   (save-excursion
+                                     (tc/get-recent-git-authors 50))))))
+
 (use-package pinentry
   :ensure t
   :config
