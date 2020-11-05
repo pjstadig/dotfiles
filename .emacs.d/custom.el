e76e2fc0a0cb21c61521de1ca81f729895e50bdc
Paul Stadig <paul@stadig.name>
2020-11-05 09:58:10 -0500

    Try a breadcrumb that works on OSX, too

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(dired-clean-confirm-killing-deleted-buffers nil)
- '(org-agenda-breadcrumbs-separator "⏵")
+ '(org-agenda-breadcrumbs-separator "▶")
  '(org-agenda-compact-blocks t)
  '(org-agenda-custom-commands
    '(("i" "Inbox" tags "FLAGGED|CATEGORY=\"IN\"+SCHEDULED=\"\"+DEADLINE=\"\""
